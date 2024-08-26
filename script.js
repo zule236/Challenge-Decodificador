cbbd26627c0a228f2a94111153c6222afae14b59
zule236 <mzuleimanarvaez@gmail.com>
2024-08-26 17:59:08 -0500

    Add files via upload
    Desafío decodificador.

---
+++ script.js
@@ -0,0 +1,59 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar(){
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptada;
+}
+
+function copiarTexto() {
+    const mensaje = document.querySelector(".mensaje");
+
+    // Seleccionar el contenido del textarea
+    mensaje.select();
+    mensaje.setSelectionRange(0, 99999); // Para dispositivos móviles
+
+    // Copiar el texto al portapapeles
+    document.execCommand("copy");
+
+    // (Opcional) Mostrar una alerta o notificación de que el texto ha sido copiado
+    alert("Texto copiado al portapapeles");
+}
